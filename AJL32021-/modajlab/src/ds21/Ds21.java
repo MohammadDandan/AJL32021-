bb522433f51bf884e135ec9aca75657f86227015
MohammadDandan <73352550+MohammadDandan@users.noreply.github.com>
2020-11-12 14:21:39 +0200

    Add files via upload

---
+++ AJL32021-/modajlab/src/ds21/Ds21.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.
+package ds21;
+
+/**
+ *
+ * @author mod
+ */
+public class Ds21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        // TODO code application logic here
+    }
+    
+}
